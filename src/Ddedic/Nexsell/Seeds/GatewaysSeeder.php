b3659698421ee7f3396de6c05957974bd09e6372
Danijel Dedic <dedic.c@gmail.com>
2013-10-29 15:31:30 +0100

    test test test

--- src/Ddedic/Nexsell/Seeds/GatewaysSeeder.php
+++ src/Ddedic/Nexsell/Seeds/GatewaysSeeder.php
@@ -13,8 +13,8 @@ class GatewaysSeeder extends Seeder {
      */
     public function run() {
 
-        $gateways = [
-            [
+        $gateways = array(
+            array(
                 'name'              => 'Nexmo (default)',
                 'description'       => 'Nexmo defult gateway',
                 'class_name'        => 'NexmoGateway',
@@ -23,8 +23,8 @@ class GatewaysSeeder extends Seeder {
                 'active'            =>  1,
                 'created_at'        => date('Y-m-d H:i:s'),
                 'updated_at'        => date('Y-m-d H:i:s')
-            ]
-        ];
+            )
+        );
         DB::table('gateways')->insert($gateways);
         $this->command->info('Gateways Table Seeded');
 
